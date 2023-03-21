25e4b95bf2162d86f8424bf14cf45db361d209df
namotoy <namotopet0615@gmail.com>
2023-03-21 22:03:07 +0900

    edit sample.java

--- Sample.java
+++ Sample.java
@@ -4,3 +4,29 @@ class Sample {
         System.out.print(text);
     }
 }
+
+class Sample2 {
+    public static void main(String[] args) {
+        int number = 5;
+        String text = "ああああ";
+        boolean flag = true;
+
+        System.out.println(number);
+        System.out.println(text);
+        System.out.println(flag);
+    }
+}
+
+class Sample3 {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 5;
+
+        System.out.println(number1+number2);
+        System.out.println(number1-number2);
+        System.out.println(number1*number2);
+        System.out.println(number1/number2);
+        System.out.println(number1%number2);
+    }
+}
+
